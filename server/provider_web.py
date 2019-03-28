062bc571d1310855e5e7683f0a715d79631a91b7
Rickard Hallerbäck <rickard_hallerback@hotmail.com>
2019-03-28 20:13:14 +0100

    using input()

--- server/provider_web.py
+++ server/provider_web.py
@@ -3,8 +3,8 @@ import zmq
 import datetime
 import socket as sp
 
-HOST = '192.168.68.104'  # Standard loopback interface address (localhost)
-PORT = 7680        # Port to listen on (non-privileged ports are > 1023)
+HOST = 'localhost' # Change to WAN before using. 
+PORT = 7680     
 
 PROVIDER_SOCKET = "ipc:///tmp/provider_ipc"
 
@@ -13,6 +13,14 @@ if __name__=="__main__":
     socket = context.socket(zmq.SUB)
     socket.setsockopt_string(zmq.SUBSCRIBE, "")
     socket.connect (PROVIDER_SOCKET)
+
+    print("Make sure your reachable, if your behind a NAT router (such as a WiFi access point) do port forwarding.")
+    HOST = input("Enter you WAN adress, make sure its reachable:\n")
+    try:
+        PORT = int(input("Enter the port your hosting the service on."))
+    except ValueError:
+        print("Please a valid port number.")
+        exit(1)
 
     with sp.socket(sp.AF_INET, sp.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
